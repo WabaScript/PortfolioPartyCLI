09e8c2700eb2ef5110a62077e7fbf0b700af2d9f
haihuanchen <haihuan.chen@hotmail.com>
2020-02-11 15:38:36 -0500

    sam's changes

--- run.rb
+++ run.rb
@@ -1,6 +1,13 @@
 require_relative "./config/environment"
 
+def welcome_message
+    puts "Welcome to the Portfolio Generator!"
+end
 
+def log_in
+    puts "Press 1 to Create a new user, or press 2 to log in: "
+    input = gets.chomp
+end
 
 binding.pry
 0
