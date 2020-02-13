44dfead65555e8d085761f622b33af5cd01f3687
wags83 <gpwagner@gmail.com>
2020-02-13 15:07:16 -0500

    Feature complete

--- lib/investment.rb
+++ lib/investment.rb
@@ -4,7 +4,6 @@ class Investment < ActiveRecord::Base
     belongs_to :portfolio
 
     #Method works fine, but API limits calls to 5 per minute so it will fail on a large table
-    
     def self.update_all_prices_to_current
         Investment.all.each do |investment|
             investment.update(current_price: get_current_stock_price(investment.symbol))
@@ -46,6 +45,7 @@ class Investment < ActiveRecord::Base
     end
 
     def investment_value
-        self.
+        self.num_shares * self.current_price
+    end
 
 end
--- run.rb
+++ run.rb
@@ -1,7 +1,7 @@
 require_relative "./config/environment"
 
 #-----Pre-run Data Load-----#
-#Before demo run "Investment.update_all_prices_to_current_slowly_better"
+#Before demo, run "rake update_prices" in the terminal
 #This will update all investment values to the current day value
 #This only needs to be done once per day, but will take several minutes due to the 5 API calls per minute limit
 #(Total number of investments / 5 minutes)
